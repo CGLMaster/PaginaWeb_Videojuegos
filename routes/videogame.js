14c8be2b3fb32abb365500223150a5a2ce5aa4db
CGLMaster <cgomez22@ucm.es>
2023-02-01 23:17:39 +0100

    Animaciones en Main

--- routes/videogame.js
+++ routes/videogame.js
@@ -7,9 +7,9 @@ const games = require('../API/games.json');
 
 /* GET users listing. */
 router.get('/', async(request, response) => {
-    const newGames = await Videogame.find({rating: {$gt:88}, first_release_date : {$gt: 1640991600}, aggregated_rating_count: {$gt: 8}}).sort({first_release_date : -1}).limit(4);
+    const bestGames = await Videogame.find({rating: {$gt:88}, first_release_date : {$gt: 1640991600}, aggregated_rating_count: {$gt: 8}}).sort({first_release_date : -1}).limit(4);
     const topGames = await Videogame.find({rating: {$gt:80}, first_release_date : {$gt: 1640991600}, aggregated_rating_count: {$gt: 8}}).sort({first_release_date : -1}).limit(15);
-    response.render("main", {"newGames" : newGames, "topGames": topGames});
+    response.render("main", {"bestGames" : bestGames, "topGames": topGames});
     //response.json(videogames);
 });
 
